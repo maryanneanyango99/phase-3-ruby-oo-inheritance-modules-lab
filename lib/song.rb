eaba9a4d6bb4a997ded36791b1b63f2cc9eeb51f
Moringa School <maryanneanyango98@gmail.com>
2022-09-02 16:00:12 +0300

    commit tree 45cb0ee3030f57f398b6af8dd663bc94a175a1cc

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,12 @@
 require 'pry'
+# require_relative './memorable.rb'
+require_relative '../lib/concerns/memorable'
 
 class Song
   attr_accessor :name
   attr_reader :artist
+
+  extend Memorable
 
   @@songs = []
 
@@ -18,13 +22,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
